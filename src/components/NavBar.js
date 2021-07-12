5cc4744e6c8a245b15172d9a20e70b6972dd7f38
ltorchioi <ltorchioi@glamit.com.ar>
2021-07-12 00:33:30 -0300

    se crea carpeta components + navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import {NavDropdown, Navbar, Nav} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.png'
+
+function Header() {
+
+    return (
+      <div className="App" >
+        <Navbar bg="light" expand="lg">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto"> 
+            <NavDropdown title="Menú" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Entradas</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Pizzas</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Empanadas</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#link">Nosotros</Nav.Link>
+            <Nav.Link href="#home">Contacto</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+      </div>
+    )
+  }
+
+
+export default Header
