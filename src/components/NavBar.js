6665125da357cec8562fd0f1a0cb7b858603c19b
ltorchioi <ltorchioi@glamit.com.ar>
2021-07-14 10:15:26 -0300

    se modifica logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,8 @@
 import {NavDropdown, Navbar, Nav} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.png'
+import logo from './logo.svg'
 
-function NavBar() {
+const NavBar = () => {
 
     return (
         <Navbar bg="light" expand="lg">
@@ -11,13 +11,13 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto"> 
-            <NavDropdown title="Menú" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Entradas</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Pizzas</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Empanadas</NavDropdown.Item>
+            <NavDropdown title="Eshop" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Remeras</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Pantalones</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Abrigos</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#link">Nosotros</Nav.Link>
-            <Nav.Link href="#home">Contacto</Nav.Link>
+            <Nav.Link href="#link">Denim</Nav.Link>
+            <Nav.Link href="#home">Girls</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
